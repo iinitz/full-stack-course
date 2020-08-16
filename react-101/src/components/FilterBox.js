832d4b66b9ef68eb97d11ad946e0c3bc758a4242
Saksit Sawang <hanamiza555@gmail.com>
2020-08-16 16:03:58 +0700

    example hooks

--- react-101/src/components/FilterBox.js
+++ react-101/src/components/FilterBox.js
@@ -1,15 +1,38 @@
-import React, { useState, Fragment } from 'react'
+import React, { useState, Fragment, useMemo, useCallback, useEffect } from 'react'
 import PostList from './PostList'
 
 const FilterBox = ({ posts }) => {
   const [text, setText] = useState('')
-  const handleTextChange = (e) => {
-    setText(e.target.value)
-  }
-  const filteredPosts = posts.filter((post) => {
-    const patt = new RegExp(text, 'ig')
-    return text === '' || patt.test(post.title) || patt.test(post.content)
-  })
+  // const [data, setData] = useState([])
+  // setData([1, 2 ,3])
+  // setData([...data, newRecord]) // wrong data
+  // setData((prev) => {
+  //   return [...prev, newRecord]
+  // })
+  const handleTextChange = useCallback(
+    (e) => {
+      setText(e.target.value)
+    },
+    [setText],
+  )
+  const filteredPosts = useMemo(
+    () => {
+      return posts.filter((post) => {
+        const patt = new RegExp(text, 'ig')
+        return text === '' || patt.test(post.title) || patt.test(post.content)
+      })
+    },
+    [posts, text],
+  )
+  useEffect(
+    () => {
+      console.log('didMount')
+      return () => {
+        console.log('willUnmount')
+      }
+    },
+    [],
+  )
   return (
     <Fragment>
       Filter: <input value={text} onChange={handleTextChange} />
