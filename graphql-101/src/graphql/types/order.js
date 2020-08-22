8ca2addc0210eff9eda318fe68c62f679ad6abf4
iinitz <hanamiza555@gmail.com>
2020-08-22 14:49:15 +0700

    fix args

--- graphql-101/src/graphql/types/order.js
+++ graphql-101/src/graphql/types/order.js
@@ -8,6 +8,7 @@ export const OrderType = composeWithMongoose(OrderModel)
 OrderType.addRelation('orderItems', {
   resolver: () => ProductType.getResolver('findByIds'),
   prepareArgs: {
+    // filter: source => ({ _ids: source.products.map(item => item.productId)}),
     _ids: source => source.products.map(item => item.productId)
   },
   projection: { products: 1 },
