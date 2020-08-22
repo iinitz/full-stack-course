5e0ac00e1c67b645072df99bf30a79925242f6ad
iinitz <hanamiza555@gmail.com>
2020-08-22 15:32:35 +0700

    update config

--- graphql-101/src/connection.js
+++ graphql-101/src/connection.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose'
 
-mongoose.connect('mongodb://localhost:27017', {
-  // auth: {
-  //   user: 'root',
-  //   password: '1234',
-  // },
-  // dbName: 'db',
-  // useNewUrlParser: true,
-  // useUnifiedTopology: true,
+mongoose.connect('mongodb://mongodb:27017', {
+  auth: {
+    user: 'user',
+    password: 'pass',
+  },
+  dbName: 'db',
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
