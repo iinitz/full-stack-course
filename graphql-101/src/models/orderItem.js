514d6edf00f28159f6ca1f9d10f517bcc4f74f69
iinitz <hanamiza555@gmail.com>
2020-08-23 11:09:36 +0700

    update component

--- graphql-101/src/models/orderItem.js
+++ graphql-101/src/models/orderItem.js
@@ -4,7 +4,6 @@ const OrderItemSchema = new mongoose.Schema({
   orderId: { type: String, required: true },
   productId: { type: String, required: true },
   quantity: { type: Number, required: true },
-  price: { type: Number, required: true },
 })
 
 export const OrderItemModel = mongoose.model('OrderItem', OrderItemSchema)
---
+++ nextjs-101/src/pages/products.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import ProductList from '../components/ProductList'
+
+const Products = () => {
+  return (
+    <ProductList />
+  )
+}
+
+export default Products
