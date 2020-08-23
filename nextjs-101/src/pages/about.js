9465dfb78494ea303491d8cee5176da6b3f8d33b
iinitz <hanamiza555@gmail.com>
2020-08-23 10:38:31 +0700

    move files

--- nextjs-101/src/pages/about.js
+++ nextjs-101/src/pages/about.js
@@ -3,9 +3,9 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import ProTip from '../components/ProTip';
+import Link from '../components/Link';
+import Copyright from '../components/Copyright';
 
 export default function About() {
   return (
--- nextjs-101/src/pages/index.js
+++ nextjs-101/src/pages/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import ProTip from '../components/ProTip';
+import Link from '../components/Link';
+import Copyright from '../components/Copyright';
 
 export default function Index() {
   return (
